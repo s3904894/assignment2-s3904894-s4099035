0e4c126de914f82ac1bc0cc3dda85511f775754e
s3904894 <s3904894@student.rmit.edu.au>
2025-10-17 22:14:23 +1100

    Habood Final Product

---
+++ HaboodWidget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  HaboodWidget
+//
+//  Created by yunlong chen on 2025/10/17.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource { "Configuration" }
+    static var description: IntentDescription { "This is an example widget." }
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
